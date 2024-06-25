92038dab37792c7de895c86e420377b476e5a4e3
Simon Marcel Linden <linden@schulz-hygiene.de>
2024-06-25 15:35:08 +0200

    RX und TX Pins hinzugefügt und Fehlerbehandlung verbessert
      - src/main.cpp:
      - RX und TX Pins hinzugefügt (rxPin und txPin).
      - Fehlerbehandlung für die Initialisierung von Serial und mySerial hinzugefügt.
      - Kommentierte Codezeilen für das Setzen der Pins auf INPUT_PULLUP hinzugefügt.
      - Debug-Ausgabe bei erfolgreicher Baudratenaktualisierung hinzugefügt.
      - RX- und TX-Pins im Loop überprüft, um die LED entsprechend zu blinken.
      - Gelbe LED blinkt, wenn keine serielle Kommunikation stattfindet.
      - Puffer wird am Ende der Loop-Funktion geleert.
    
    Diese Änderungen verbessern die Handhabung der seriellen Kommunikation und die Fehlerbehandlung im System.

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,14 @@ const int redLedPin     = 27;
 const int yellowLedPin  = 14;
 const int greenLedPin   = 12;
 
+// RX und TX Pins definieren
+const int rxPin = 16;
+const int txPin = 17;
+
 void clearSerialBuffer() {
-    while (Serial.available() > 0) {
-        Serial.read();
-    }
+	while (Serial.available() > 0) {
+		Serial.read();
+	}
 }
 
 /**
@@ -19,17 +23,21 @@ void setup() {
 	// Initialisierung der eigenen seriellen Schnittstellen
 	Serial.begin(9600);
 	if (!Serial) {
-        handleError("Serial initialization failed");
-        return;
-    }
+		handleError("Serial initialization failed");
+		return;
+	}
 
-	mySerial.begin(1200, SERIAL_8N1, 17, 16); // RX1, TX1
+	mySerial.begin(1200, SERIAL_8N1, rxPin, txPin); // RX1, TX1
+	// Setze die Pins auf INPUT_PULLUP und überprüfe den Zustand
+	// pinMode(rxPin, INPUT);
+	// pinMode(txPin, INPUT);
+
 	if (!mySerial) {
-        handleError("mySerial initialization failed");
-        return;
-    }
+		handleError("mySerial initialization failed");
+		return;
+	}
 
-    BLEDevice::init("Schulz-BT-ESP32");
+	BLEDevice::init("Schulz-BT-ESP32");
 
 	// Konfiguration der Drehschalter-Pins als Eingänge
 	for (int i = 0; i < 8; i++) {
@@ -43,55 +51,54 @@ void setup() {
 	}
 
 	std::string deviceName = "Schulz BT - " + BLEDevice::getAddress().toString();
-    setupBLE(deviceName);
+	setupBLE(deviceName);
 	DEBUG_PRINT("Device Name: " + deviceName);
-
-	setLedState(redLedPin, LOW);
-	setLedState(yellowLedPin, LOW);
-	setLedState(greenLedPin, LOW);
 }
 
 void loop() {
-    // Lesen des Drehschalterzustands
+	// Lesen des Drehschalterzustands
 	int switchPosition = readSwitchPosition();
 
 	if (switchPosition != currentSwitchPosition && switchPosition != -1) {
-        currentSwitchPosition = switchPosition;
-        mySerial.updateBaudRate(baudRates[switchPosition]);
-        DEBUG_PRINT("Baudrate updated to: " + String(baudRates[switchPosition]));
-    }
+		currentSwitchPosition = switchPosition;
+		mySerial.updateBaudRate(baudRates[switchPosition]);
+		DEBUG_PRINT("Baudrate updated to: " + String(baudRates[switchPosition]));
+	}
 
 	if (mySerial.available() > 0) {
 		// Nachricht lesen
 		String message = mySerial.readStringUntil('\n');
 		if (message.length() == 0) {
-            handleError("Empty message received from mySerial");
-            return;
-        }
+			handleError("Empty message received from mySerial");
+			return;
+		}
 		Serial.println(message);
-   		digitalWrite(redLedPin, LOW);
-		blinkLed(yellowLedPin, 1000);
 
 		// Nachricht über Bluetooth senden
 		if (btDeviceConnected) {
 			pCharacteristic->setValue(message.c_str());
 			pCharacteristic->notify();
-			// blinkLed(greenLedPin, 500);
 		}
-   		digitalWrite(greenLedPin, HIGH);
-	} else if (mySerial.available() == 0)  {
-		// Blinken der gelben LED, wenn keine serielle Kommunikation stattfindet
-   		digitalWrite(redLedPin, HIGH);
 	}
 
 	if (Serial.available()) {
 		String input = Serial.readStringUntil('\n');
 		input.trim();
 		if (input.length() > 0) {
-            mySerial.println(input);
-        }
+			mySerial.println(input);
+		}
 	}
 
+	int rxState = digitalRead(rxPin);
+	int txState = digitalRead(txPin);
+
+	if (rxState == HIGH && txState == HIGH) {
+		// Gelbe LED blinken lassen, wenn keine serielle Kommunikation stattfindet
+        blinkLed(yellowLedPin, 500);
+		setLedState(redLedPin, LOW);
+	} else {
+		setLedState(redLedPin, HIGH);
+	}
 	// Puffer leeren
 	clearSerialBuffer();
 
